7671c3300edfe21f5188f848da66d0f33c669981
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-11-02 10:20:47 -0400

    add file

---
+++ hw5-4.py
@@ -0,0 +1,51 @@
+# Author MB 11/02/2021
+
+# Question 1
+word = "Pyton"
+print(word[2])
+
+# Question 2
+word1 = "Strings are simply a sequence of characters."
+print(word1[30])
+
+# Question 3
+print("\"excellent\"")
+
+# Question 4
+word2 = "Pineapple"
+
+if word2[4:] == "apple":
+    print("true")
+else:
+    print("false")
+
+# Question 5
+word3 = "Peach"
+word4 = "pear"
+
+if word3 < word4:
+    print(word3 + " " + "comes before" + " " + word4)
+elif word3 > word4:
+    print(word3 + " " + "comes after" + " " + word4)
+else:
+    print("words are the same.")
+
+# Question 6
+print('x' * 20)
+
+# Question 7
+num = 54321
+print(str(num))
+
+# Question 8
+word2 = "Pineapple"
+
+if word2[4:] == "p":
+    print("true")
+else:
+    print("false")
+
+# Question 9
+print("\"Livin' On a Prayer\" is my friend's favorite song.")
+# Question 10
+
